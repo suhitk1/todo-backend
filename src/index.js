03ae83f1e207603c35fdc5ed10e21847adac3ae0
suhitk1 <suhitk2112@gmail.com>
2021-01-28 15:28:24 +0530

    included cookie-session

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const bcrypt = require("bcrypt");
-const session = require("express-session");
+const session = require("cookie-session");
 
 const session_secret = "newton";
 
